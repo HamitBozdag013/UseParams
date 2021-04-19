c9ece5856ac2d48aeae5acfb44b9f429c87f4a26
Hamit Bozdağ <bozdag.013@gmail.com>
2021-04-19 06:08:28 +0300

    Add project files.

---
+++ UseParams/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace UseParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(13,27,45,80,35)); //istediğimiz kadar sayı gönderebiliriz..
+        }
+
+        static int Add(params int[] numbers) //params buradaki Add metodundaki diziyi dinamik hale getirir. İstediğimiz kadar verilen sayıları taoplamamızı sağlar.
+        {
+            return numbers.Sum(); //number degiskeninin degerlerini toplar        
+        }
+    }
+}
